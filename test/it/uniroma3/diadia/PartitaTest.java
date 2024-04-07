2924df6503f0a3e9dcb07bd088ec35976a9ba23d
EdoardoFornelli <edo.fornelli@stud.uniroma3.it>
2024-04-07 15:35:47 +0200

    push 07-04 ore 15:35
    Finite classi di test per stanza e partita

--- test/it/uniroma3/diadia/PartitaTest.java
+++ test/it/uniroma3/diadia/PartitaTest.java
@@ -1,28 +1,44 @@
 package it.uniroma3.diadia;
 
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.giocatore.Giocatore;
 
 public class PartitaTest {
 	
 	private Partita partita;
+	private Giocatore giocatore;
+	private Stanza stanzaCorrente;
 	
 	@BeforeEach
+	public void setUp () {
+		this.partita = new Partita();
+		this.giocatore = new Giocatore ();
+		this.stanzaCorrente = new Stanza("sala giochi");
+		
+		giocatore.setCfu(-20);
+		partita.setGiocatore(giocatore);
+		partita.setStanzaCorrente(stanzaCorrente);
+	}
+	
 	
 	@Test
 	public void testIsFinita() {
-		fail("Not yet implemented");
+		assertTrue(this.partita.isFinita());
 	}
 	
 	@Test
-	public void testVinta() {
-		fail("Not yet implemented");
+	public void testSetStanzaCorrente() {
+		assertEquals(stanzaCorrente, partita.getStanzaCorrente());
 	}
 
 	@Test
-	public void testGetGiocatore() {
-		fail("Not yet implemented");
+	public void testSetGiocatore() {
+		assertEquals(giocatore, partita.getGiocatore());
 	}
 }
